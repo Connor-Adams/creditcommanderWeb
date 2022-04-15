a8687a56be3e9eed0c30862308c5ea8f2172400f
dominicols <dominic_ols@hotmail.com>
2022-04-15 17:35:18 -0400

    Amazon download link

--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -40,7 +40,7 @@ const InfoSection = ({lightBg, id, imgStart, topLine, lightText, headLine,darkTe
                                 primary={primary ? 1 : 0}
                                 dark={dark ? 1 : 0}
                                 dark2={dark2 ? 1: 0}
-                                secondary={secondary ? 1 : 0}  to='//play.google.com/store/apps?&utm_source=na_Med&utm_medium=hasem&utm_content=Oct0120&utm_campaign=Evergreen&pcampaignid=MKT-EDR-na-ca-1000189-Med-hasem-py-Evergreen-Oct0120-Text_Search_BKWS-id_101415_%7cEXA%7cONSEM_kwid_43700057579875204&gclid=CjwKCAjwur-SBhB6EiwA5sKtjgUXc0x0AhEzRJrHIg7p5icKkJfv1sNNo3GTk5-Xu9Q6GLQ0OLOmVBoC6fUQAvD_BwE&gclsrc=aw.ds' target="_blank">
+                                secondary={secondary ? 1 : 0}  to='//www.amazon.com/gp/product/B09XTHNZ9T' target="_blank">
                          {linkLabel}
                      </NavBtnLink>
                  </NavBtn>
